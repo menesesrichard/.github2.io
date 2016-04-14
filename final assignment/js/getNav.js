44db5e0350b9fb35f79610233cc39487f086d175
menesesrichard <Rich@Richards-MacBook-Pro.local>
2016-04-13 23:40:30 -0400

    stuff

---
+++ final assignment/js/getNav.js
@@ -0,0 +1,11 @@
+$.get("", function(data) {
+
+    $(document).ready(function(){
+    $(".container").prepend(data); //sends the data from nav.html to the start of the container class.
+    
+
+   
+    })
+
+
+})
